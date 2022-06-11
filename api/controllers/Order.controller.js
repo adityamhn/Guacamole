74158ec157ea59d9056b861645521250f7d01b94
haseebkhalid1507 <haseebkhalid1507@gmail.com>
2022-06-11 18:17:22 +0530

    fixed price calc

--- api/controllers/Order.controller.js
+++ api/controllers/Order.controller.js
@@ -5,60 +5,71 @@ const jwt = require("jsonwebtoken");
 const ItemModel = require("../models/Item.model");
 
 exports.AddOrder = async (req, res, next) => {
-    const { u_id, r_id, items } = req.body;
+  const { u_id, r_id, items } = req.body;
 
-    if (!u_id || !r_id || items)
+  if (!u_id || !r_id || items)
     return res.status(500).json({
       success: false,
       message: "Required values not provided!",
     });
 
-    var price = 0;
-    
+  const itemPrices = await ItemModel.find({
+    _id: {
+      $in: items,
+    },
+  });
 
-    const newOrder = new OrderModel(req.body);
+  var price = 0;
+  itemPrices.forEach((item) => {
+    price += item.price;
+  });
 
-    newOrder
-      .save()
-      .then(async (n) => {
-        return res.status(200).json({
-            success: true,
-        });
-      })
-      .catch((err) => {
-          console.log("Error!")
-          console.log(err);
-          return res.status(500).json({
-              success:false,
-              message: "Unknown server error."
-          });
+  const newOrder = new OrderModel({
+    u_id,
+    r_id,
+    items,
+    price,
+  });
+
+  newOrder
+    .save()
+    .then(async (n) => {
+      return res.status(200).json({
+        success: true,
       });
-
-
+    })
+    .catch((err) => {
+      console.log("Error!");
+      console.log(err);
+      return res.status(500).json({
+        success: false,
+        message: "Unknown server error.",
+      });
+    });
 };
 
 exports.GetOrderDetails = (req, res, next) => {
-    const { _id } = res.body;
-    if (!_id)
-        return res.status(500).json({
-        success: false,
-        message: "Required values not provided!",
-        });
-    return OrderModel.findById({
-        _id,
+  const { _id } = res.body;
+  if (!_id)
+    return res.status(500).json({
+      success: false,
+      message: "Required values not provided!",
+    });
+  return OrderModel.findById({
+    _id,
+  })
+    .then((order) => {
+      return res.status(200).json({
+        success: true,
+        order: order,
+      });
     })
-        .then((order) => {
-        return res.status(200).json({
-            success: true,
-            order: order,
-        });
-        })
-        .catch((err) => {
-        console.log("error");
-        console.log(err);
-        return res.status(500).json({
-            success: false,
-            message: "Unknown server error!",
-        });
-        });
-    };
+    .catch((err) => {
+      console.log("error");
+      console.log(err);
+      return res.status(500).json({
+        success: false,
+        message: "Unknown server error!",
+      });
+    });
+};
