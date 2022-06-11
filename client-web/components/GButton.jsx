d8ecebfc5350994e8b4dfcd9e9fa4bb06ba8ec7a
Aditya Mohan <aditya530026@gmail.com>
2022-06-11 22:51:27 +0530

    web interface ui done

---
+++ client-web/components/GButton.jsx
@@ -0,0 +1,21 @@
+import { Button } from 'antd'
+import React from 'react'
+import Styles from "/styles/components/GButton.module.scss"
+
+const GButton = ({ children, secondary, className, ...props }) => {
+    return (
+        <Button
+            className={`${Styles.GButton} ${secondary && Styles.GButtonSecondary} ${className}`}
+            onClick={props.onClick}
+            style={props.style}
+            loading={props.loading}
+            icon={props.icon}
+            ghost={props.ghost}
+            disabled={props.disabled}
+            danger={props.danger}
+            htmlType={props.htmlType}
+        >{children}</Button>
+    )
+}
+
+export default GButton
