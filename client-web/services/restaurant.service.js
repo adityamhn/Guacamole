c84272d2e0c555d7af3043503b8d51dbc2dc6d92
haseebkhalid1507 <haseebkhalid1507@gmail.com>
2022-06-12 01:06:50 +0530

    some frontend integrarions

---
+++ client-web/services/restaurant.service.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import store from "../app/store";
+import { BACKEND_URL } from "../constants";
+
+export const GetAllRestaurants = async () => {
+  return await axios.get(`${BACKEND_URL}/api/restaurant/get-all-restaurants`, {
+    headers: {
+      token,
+    },
+  });
+};
+
+export const SignInRestaurant = (body) => {
+  return axios.post(`${BACKEND_URL}/api/restaurant/sign-in`, body);
+};
+
+export const SignUpRestaurant = async (body) => {
+  return await axios.post(`${BACKEND_URL}/api/restaurant/sign-up`, body);
+};
+
+export const UpdateRestaurantDetails = (body) => {
+  const { token } = store.getState().auth;
+  if (!token) throw new Error("Token not found!");
+  return axios.put(
+    `${BACKEND_URL}/api/restaurant/update-restaurant-details`,
+    body,
+    {
+      headers: {
+        token,
+      },
+    }
+  );
+};
+
+export const CheckSignedIn = () => {
+  const { token } = store.getState().auth;
+  return axios.get(`${BACKEND_URL}/api/restaurant/check-signed-in`, {
+    headers: {
+      token,
+    },
+  });
+};
+
+export const GetRestaurantDetails = (_id) => {
+  const { token } = store.getState().auth;
+  if (!token) throw new Error("Token not found!");
+  return axios.get(`${BACKEND_URL}/api/restaurant/get-restaurant-details`, {
+    headers: {
+      token,
+    },
+  });
+};
