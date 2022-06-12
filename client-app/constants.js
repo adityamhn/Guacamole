88247cc95d7abe88d2ac0855d956cc955b95efd5
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-06-12 12:25:38 +0530

    Base URL Changed

--- client-app/constants.js
+++ client-app/constants.js
@@ -5,4 +5,5 @@ export const WHITE_SECONDARY = "#B5B5B5";
 export const WHITE = "#F3F3F3";
 export const PRIMARY_FONT = "Dinosaur";
 export const SECONDARY_FONT = "Poppins";
-export const BACKEND_URL = "https://733d-14-97-167-154.in.ngrok.io";
+export const BACKEND_URL =
+  "http://ec2-3-108-235-208.ap-south-1.compute.amazonaws.com:8010";
--- client-web/constants.js
+++ client-web/constants.js
@@ -1,3 +1,2 @@
 export const BACKEND_URL =
-  // "http://ec2-3-108-235-208.ap-south-1.compute.amazonaws.com:8010";
-  "https://733d-14-97-167-154.in.ngrok.io";
+  "http://ec2-3-108-235-208.ap-south-1.compute.amazonaws.com:8010";
